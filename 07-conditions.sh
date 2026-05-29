28ebc823fe34249708b285c6537e2b846e5f0b06
Lakshmareddych29 <lakshmareddychilukuri@gmail.com>
2026-05-29 08:24:42 +0530

    hello world

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 NUMBER=$1
+# gt - greaterthan
+# lt - lessthan
+# eq - equal 
+# ne - not equal
+# ge - greater than or equal
+# le - lessthan or equal
 
-if [ $NUMBER -gt 20 ]; then
- echo "then $NUMBER is greater than 20"
+
+if [ $NUMBER -ge 20 ]; then
+ echo "then $NUMBER is greater than or equal to 20"
 else 
- echo "then the $NUMBER is less than 20"
+ echo "then the $NUMBER is less than or equal to 20"
 fi
